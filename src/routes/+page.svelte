e224f3ecab3ea0b2f49fb8d1b5f886011ae4fbc7
Snoilt <paul@oellers.net>
2023-04-15 21:30:55 +0200

    some refactoring

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,10 +3,6 @@
     import { currentUser, pb, signOut } from "$lib/pocketbase"
     import { onMount } from "svelte";
     import { goto } from '$app/navigation';
-
-    
-
-
     onMount(()=>{
         if ($currentUser) {
             goto ("/CarSelection")
@@ -15,27 +11,4 @@
         )
     })
   </script>
-  
-  
-  
-  
-  <main>  
-  </main>
-  
-  <style>
-    #headLine {
-      position: absolute;
-      top: 2rem;
-      left: 0;
-      right: 0;
-      text-align: center;
-      margin: 0;
-      padding: 0;
-      font-size: 3rem;
-      font-weight: 700;
-      color: #1b1b1b;
-      padding: 1rem;
-      margin-bottom: 30px;
-    }
-  </style>
   
