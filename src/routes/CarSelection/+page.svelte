1ebf5d1ffd98b85620c7fa968f3f139eb9b7b358
Snoilt <paul@oellers.net>
2023-04-16 16:26:01 +0200

    fix data updating

--- src/routes/CarSelection/+page.svelte
+++ src/routes/CarSelection/+page.svelte
@@ -6,7 +6,6 @@
 
   export let data
   let cars = data.cars
-  console.log(cars)
   let carInput: HTMLInputElement
 
   async function addCar() {
@@ -15,23 +14,21 @@
       carName: carInput.value,
       user: data.user,
     }
-    carCollection.create(carAddData)
+    await carCollection.create(carAddData)
     invalidateAll()
-    //I do hate this but for now i am jsut so confused 
-    window.location.reload()
   }
+  $: cars = data.cars
 </script>
 
-<main />
-
-{#each cars as car}
-  <button><a href="/CarOverview/{car.carName}">{car.carName}</a></button>
-{/each}
-
-<div>
-  <input type="text" placeholder="Car Name" bind:this={carInput} />
-  <button on:click={addCar}>Add Car</button>
-</div>
+<main>
+  {#each $cars as car}
+    <button><a href="/CarOverview/{car.carName}">{car.carName}</a></button>
+  {/each}
+  <div>
+    <input type="text" placeholder="Car Name" bind:this={carInput} />
+    <button on:click={addCar}>Add Car</button>
+  </div>
+</main>
 
 <style>
 </style>
--- src/routes/CarSelection/+page.ts
+++ src/routes/CarSelection/+page.ts
@@ -2,10 +2,10 @@ import { currentUser, pb} from "$lib/pocketbase"
 import { writable } from "svelte/store"
 export const load = async () => {
     const carCollection = await pb.collection("cars")
-    console.log("loaded")
+    // console.log("loaded")
     return {
         carCollection: carCollection,
-        cars:(await carCollection.getList()).items,
+        cars: writable((await carCollection.getList()).items),
         user: pb.authStore.model?.id
     }
 }
