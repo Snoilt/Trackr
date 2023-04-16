cd0746d35b95eb8b93342a74ac0741d81080ad63
Snoilt <paul@oellers.net>
2023-04-16 20:28:05 +0200

    add edit mode

--- src/routes/CarOverview/[slug]/+page.svelte
+++ src/routes/CarOverview/[slug]/+page.svelte
@@ -1,25 +1,53 @@
 <script lang="ts">
   export let data
   import EntryCarCreator from "$lib/EntryCreator.svelte"
-  let entries = data.entries
+  import type { Record } from "pocketbase"
+  import { invalidate, invalidateAll } from "$app/navigation"
 
+  let entries = data.entries
+  let editMode = false
   let car = data.car
+
+  async function removeEntry(entry: Record) {
+    await data.collection.delete(entry.id)
+    invalidateAll()
+  }
   $: entries = data.entries
 </script>
+
 
 <main>
   <a href="/CarSelection">Go back to Overview</a>
   <h1>{car.carName}</h1>
   {#each $entries as entry}
-    <div class="entry">
-      <h3>{entry.maintenanceType}</h3>
-      <h3>{entry.shortDescription}</h3>
-      <h3>{entry.repairTime}</h3>
-      <p>{entry.kilometersDone}Km</p>
-      <p>{entry.doneText}</p>
-    </div>
+    {#if editMode}
+      <div class="entry">
+        <h3>{entry.maintenanceType}</h3>
+        <h3>{entry.shortDescription}</h3>
+        <h3>{entry.repairTime}</h3>
+        <p>{entry.kilometersDone}Km</p>
+        <p>{entry.doneText}</p>
+        <button on:click={()=>removeEntry(entry)}>delete</button>
+      </div>
+      
+    {:else}
+      <div class="entry">
+        <h3>{entry.maintenanceType}</h3>
+        <h3>{entry.shortDescription}</h3>
+        <h3>{entry.repairTime}</h3>
+        <p>{entry.kilometersDone}Km</p>
+        <p>{entry.doneText}</p>
+      </div>
+    {/if}
   {/each}
+  {#if editMode}
   <EntryCarCreator carId={data.slug} />
+  {/if}
+  <button
+  on:click={() => {
+    editMode = editMode ? false : true
+  }}>Edit</button
+>
 </main>
 
 <style>
--- src/routes/CarOverview/[slug]/+page.ts
+++ src/routes/CarOverview/[slug]/+page.ts
@@ -1,10 +1,12 @@
 import { currentUser, pb} from "$lib/pocketbase"
 import { writable } from "svelte/store"
 export const load = async ({ params }) => {
-    const maintenanceEntries = await pb.collection("maintenance_entry").getList()
+    const maintenanceEntriesCollection = await pb.collection("maintenance_entry")
+    const maintenanceEntries = await maintenanceEntriesCollection.getList()
     const car = await pb.collection("cars").getOne(params.slug)
     console.log("loaded")
     return {
+        collection: maintenanceEntriesCollection,
         slug: params.slug,
         entries: writable(maintenanceEntries.items),
         car: car
