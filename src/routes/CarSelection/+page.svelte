57bab45cfc4ba2a4cb8f5f0b31de0b71924eeb76
Snoilt <paul@oellers.net>
2023-04-16 17:02:34 +0200

    create edit mode. add option to delete cars. only open add car option in edit mode

--- src/routes/CarSelection/+page.svelte
+++ src/routes/CarSelection/+page.svelte
@@ -2,32 +2,59 @@
   import { invalidate, invalidateAll } from "$app/navigation"
   import { goto } from "$app/navigation"
   import { currentUser, pb } from "$lib/pocketbase"
+  import type { Record } from "pocketbase"
   import { load } from "../CarOverview/[slug]/+page.js"
 
   export let data
   let cars = data.cars
   let carInput: HTMLInputElement
+  const carCollection = data.carCollection
 
   async function addCar() {
-    const carCollection = data.carCollection
     let carAddData = {
       carName: carInput.value,
       user: data.user,
     }
     await carCollection.create(carAddData)
+    carInput.value = ""
     invalidateAll()
   }
+
+  
+  let editMode = false
+
+  async function removeCar(car: Record) {
+    await carCollection.delete(car.id)
+    invalidateAll()
+  }
+
+
+
   $: cars = data.cars
 </script>
 
 <main>
   {#each $cars as car}
-    <button><a href="/CarOverview/{car.carName}">{car.carName}</a></button>
+    {#if editMode}
+      <div>
+        <a href="/CarOverview/{car.carName}">{car.carName}</a>
+        <button on:click={()=> removeCar(car)}>-</button>
+      </div>
+    {:else}
+      <div><a href="/CarOverview/{car.carName}">{car.carName}</a></div>
+    {/if}
   {/each}
   <div>
-    <input type="text" placeholder="Car Name" bind:this={carInput} />
-    <button on:click={addCar}>Add Car</button>
+    {#if editMode}
+      <input type="text" placeholder="Car Name" bind:this={carInput} />
+      <button on:click={addCar}>Add Car</button>
+    {/if}
   </div>
+  <button
+    on:click={() => {
+      editMode = editMode ? false : true
+    }}>Edit</button
+  >
 </main>
 
 <style>
