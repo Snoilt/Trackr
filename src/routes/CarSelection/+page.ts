7b3d3d8e9af1729210ce1733cdc4f77e3db2a92e
Snoilt <paul@oellers.net>
2023-04-17 17:37:03 +0200

    rolled back to older commit since i fucked some things uo

--- src/routes/CarSelection/+page.ts
+++ src/routes/CarSelection/+page.ts
@@ -1,15 +1,18 @@
-import { currentUser, pb} from "$lib/pocketbase"
+import { currentUser, pb } from "$lib/pocketbase"
 import { writable } from "svelte/store"
-import { error } from '@sveltejs/kit';
+import { error } from "@sveltejs/kit"
 export const load = async () => {
+  try {
     const carCollection = await pb.collection("cars")
     // console.log("loaded")
-    if (!carCollection) {
-        throw error(404, { message: "Not found" })
-    }
+
     return {
-        carCollection: carCollection,
-        cars: writable((await carCollection.getList()).items),
-        user: pb.authStore.model?.id
+      carCollection: carCollection,
+      cars: writable((await carCollection.getList()).items),
+      user: pb.authStore.model?.id,
     }
+  } catch (e) {
+    console.log(e)
+    return error(69)
+  }
 }
