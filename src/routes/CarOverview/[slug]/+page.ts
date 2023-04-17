c859d3733b8026b007f1d500043559826ddcb220
Snoilt <paul@oellers.net>
2023-04-17 18:14:05 +0200

    fuck cloudflare page fuck cloudflare pages

--- src/routes/CarOverview/[slug]/+page.ts
+++ src/routes/CarOverview/[slug]/+page.ts
@@ -4,7 +4,7 @@ export const load = async ({ params }) => {
     const maintenanceEntriesCollection = await pb.collection("maintenance_entry")
     const maintenanceEntries = await maintenanceEntriesCollection.getList(1,50,{filter:`carConnector.id="${params.slug}"`})
     const car = await pb.collection("cars").getOne(params.slug)
-    console.log("loaded")
+    // console.log("loaded")
     return {
         collection: maintenanceEntriesCollection,
         slug: params.slug,
--- src/routes/CarSelection/+page.ts
+++ src/routes/CarSelection/+page.ts
@@ -2,7 +2,6 @@ import { currentUser, pb } from "$lib/pocketbase"
 import { writable } from "svelte/store"
 import { error } from "@sveltejs/kit"
 export const load = async () => {
-  try {
     const carCollection = await pb.collection("cars")
     // console.log("loaded")
 
@@ -11,8 +10,5 @@ export const load = async () => {
       cars: writable((await carCollection.getList()).items),
       user: pb.authStore.model?.id,
     }
-  } catch (e) {
-    console.log(e)
-    return error(450)
-  }
+
 }
