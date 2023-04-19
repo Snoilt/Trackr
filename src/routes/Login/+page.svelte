33a77214446078143139d010d7417315b2af6479
Snoilt <paul@oellers.net>
2023-04-19 18:41:58 +0200

    First design bits yay

--- src/routes/Login/+page.svelte
+++ src/routes/Login/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { currentUser, pb } from "$lib/pocketbase"
-  import { goto } from '$app/navigation';
+  import { goto, invalidateAll } from "$app/navigation"
 
   let username: string
   let password: string
@@ -9,13 +9,15 @@
   async function login() {
     submitted = true
     try {
-        const user = await pb.collection("users").authWithPassword(username, password)
-    console.log(user)
+      const user = await pb
+        .collection("users")
+        .authWithPassword(username, password)
+      console.log(user)
+      goto("/CarSelection")
     } catch (err) {
-        console.log(err)
+      console.log(err)
+      submitted = false
     }
-    goto("/CarSelection")
-
   }
   async function signUp() {
     submitted = true
@@ -28,24 +30,68 @@
       const createdUser = await pb.collection("users").create(data)
       await login()
       console.log("signed up")
+      goto("/CarSelection")
     } catch (err) {
       console.log(err)
-    }
+      submitted = false    }
   }
 </script>
 
 <main>
-  {#if (submitted == false)}
+    <h1>Trackr.</h1>
+  {#if submitted == false}
     <form on:submit|preventDefault>
-      <input placeholder="username" type="text" bind:value={username} />
-      <input placeholder="password" type="password" bind:value={password} />
+      <input
+        class="inputForms"
+        placeholder="username"
+        type="text"
+        bind:value={username}
+      />
+      <input
+        class="inputForms"
+        placeholder="password"
+        type="password"
+        bind:value={password}
+      />
     </form>
-    <button on:click={signUp}>Sign Up</button>
-    <button on:click={login}>Login</button>
+    <button class="buttonStuff" on:click={login}>Login</button>
+    <button class="buttonStuff" on:click={signUp}>Sign Up</button>
   {:else}
     <p>loading...</p>
   {/if}
 </main>
 
-<style>
+<style lang="scss">
+  $basicFont: "Avenir Next Condensed", sans-serif;
+  main {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+  }
+  .inputForms {
+    display: block;
+    margin: 10px;
+    padding: 10px;
+    border: 1px solid black;
+    border-radius: 5px;
+  }
+  ::placeholder,
+  .buttonStuff, .inputForms {
+    font-family: $basicFont;
+    font-style: italic;
+    font-weight: 900;
+    font-size: 15px;
+    letter-spacing: 0.025em;
+  }
+
+  .buttonStuff {
+    color: black;
+    border-radius: 10px;
+    border:none;
+    margin-top: 10px;
+    width: 100px;
+    height: 50px;
+  }
 </style>
